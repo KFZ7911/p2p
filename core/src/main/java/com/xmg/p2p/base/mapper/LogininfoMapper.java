ca9ad806dfeeb7054f28314976861b850b845563
kfz7911 <kfz7911@163.com>
2022-05-23 18:00:40 +0800

    core逆向工程

---
+++ core/src/main/java/com/xmg/p2p/base/mapper/LogininfoMapper.java
@@ -0,0 +1,16 @@
+package com.xmg.p2p.base.mapper;
+
+import com.xmg.p2p.base.domain.Logininfo;
+import java.util.List;
+
+public interface LogininfoMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Logininfo record);
+
+    Logininfo selectByPrimaryKey(Long id);
+
+    List<Logininfo> selectAll();
+
+    int updateByPrimaryKey(Logininfo record);
+}
