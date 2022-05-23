df25bd8ee607f6e228b352aad21cb29b20cb8664
kfz7911 <kfz7911@163.com>
2022-05-23 17:44:14 +0800

    初始化工程

---
+++ core/src/main/java/com/xmg/p2p/base/domain/Logininfo.java
@@ -0,0 +1,8 @@
+package com.xmg.p2p.base.domain;
+
+/**
+ * @author susan
+ * @create 2022-05-23 17:23
+ */
+public class Logininfo {
+}
---
+++ website/src/main/java/com/xmg/p2p/web/LogininfoController.java
@@ -0,0 +1,8 @@
+package com.xmg.p2p.web;
+
+/**
+ * @author susan
+ * @create 2022-05-23 17:24
+ */
+public class LogininfoController {
+}
